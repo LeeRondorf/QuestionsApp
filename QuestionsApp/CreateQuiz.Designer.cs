ba5734fc6f4b5a75d0a8638719c05826c0442ebd
Christopher <langx252@crk.umn.edu>
2019-04-16 10:02:46 -0500

    createQuiz done

--- QuestionsApp/CreateQuiz.Designer.cs
+++ QuestionsApp/CreateQuiz.Designer.cs
@@ -33,16 +33,24 @@
             this.quizNameBox = new System.Windows.Forms.TextBox();
             this.saveButton = new System.Windows.Forms.Button();
             this.questionControls = new System.Windows.Forms.Panel();
+            this.questionsList = new System.Windows.Forms.ListBox();
             this.questionsListLabel = new System.Windows.Forms.Label();
-            this.questionsList = new System.Windows.Forms.ListView();
             this.questionPanel = new System.Windows.Forms.Panel();
+            this.questionName = new System.Windows.Forms.TextBox();
+            this.answersList = new System.Windows.Forms.ListBox();
+            this.questionNameLabel = new System.Windows.Forms.Label();
+            this.questionTextLabel = new System.Windows.Forms.Label();
             this.answerPanel = new System.Windows.Forms.Panel();
-            this.possibleAnswerTextBox = new System.Windows.Forms.Label();
+            this.possibleAnswerLabel = new System.Windows.Forms.Label();
             this.correctAnswerBox = new System.Windows.Forms.CheckBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.possibleAnswerTextBox = new System.Windows.Forms.TextBox();
             this.possibleAnswersLabel = new System.Windows.Forms.Label();
-            this.possibleAnswers = new System.Windows.Forms.ListView();
+            this.editAnswerButton = new System.Windows.Forms.Button();
+            this.delAnswer = new System.Windows.Forms.Button();
             this.addAnswer = new System.Windows.Forms.Button();
+            this.newQuestionButton = new System.Windows.Forms.Button();
+            this.editQuestionButton = new System.Windows.Forms.Button();
+            this.delQuestion = new System.Windows.Forms.Button();
             this.saveQuestion = new System.Windows.Forms.Button();
             this.questionTextBox = new System.Windows.Forms.RichTextBox();
             this.cancelButton = new System.Windows.Forms.Button();
@@ -83,7 +91,7 @@
             // saveButton
             // 
             this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveButton.Location = new System.Drawing.Point(832, 685);
+            this.saveButton.Location = new System.Drawing.Point(828, 709);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(134, 50);
             this.saveButton.TabIndex = 1;
@@ -94,62 +102,109 @@
             // questionControls
             // 
             this.questionControls.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.questionControls.Controls.Add(this.questionsListLabel);
             this.questionControls.Controls.Add(this.questionsList);
+            this.questionControls.Controls.Add(this.questionsListLabel);
             this.questionControls.Controls.Add(this.questionPanel);
             this.questionControls.Location = new System.Drawing.Point(12, 109);
             this.questionControls.Name = "questionControls";
-            this.questionControls.Size = new System.Drawing.Size(954, 571);
+            this.questionControls.Size = new System.Drawing.Size(954, 594);
             this.questionControls.TabIndex = 2;
+            // 
+            // questionsList
+            // 
+            this.questionsList.FormattingEnabled = true;
+            this.questionsList.ItemHeight = 20;
+            this.questionsList.Location = new System.Drawing.Point(3, 52);
+            this.questionsList.Name = "questionsList";
+            this.questionsList.Size = new System.Drawing.Size(235, 504);
+            this.questionsList.TabIndex = 9;
+            this.questionsList.SelectedIndexChanged += new System.EventHandler(this.questionsList_SelectedIndexChanged);
             // 
             // questionsListLabel
             // 
             this.questionsListLabel.AutoSize = true;
-            this.questionsListLabel.Location = new System.Drawing.Point(7, 9);
+            this.questionsListLabel.Location = new System.Drawing.Point(3, 29);
             this.questionsListLabel.Name = "questionsListLabel";
             this.questionsListLabel.Size = new System.Drawing.Size(132, 20);
             this.questionsListLabel.TabIndex = 2;
             this.questionsListLabel.Text = "List of Questions:";
             // 
-            // questionsList
-            // 
-            this.questionsList.Location = new System.Drawing.Point(3, 32);
-            this.questionsList.Name = "questionsList";
-            this.questionsList.Size = new System.Drawing.Size(235, 531);
-            this.questionsList.TabIndex = 1;
-            this.questionsList.UseCompatibleStateImageBehavior = false;
-            // 
             // questionPanel
             // 
+            this.questionPanel.Controls.Add(this.questionName);
+            this.questionPanel.Controls.Add(this.answersList);
+            this.questionPanel.Controls.Add(this.questionNameLabel);
+            this.questionPanel.Controls.Add(this.questionTextLabel);
             this.questionPanel.Controls.Add(this.answerPanel);
             this.questionPanel.Controls.Add(this.possibleAnswersLabel);
-            this.questionPanel.Controls.Add(this.possibleAnswers);
+            this.questionPanel.Controls.Add(this.editAnswerButton);
+            this.questionPanel.Controls.Add(this.delAnswer);
             this.questionPanel.Controls.Add(this.addAnswer);
+            this.questionPanel.Controls.Add(this.newQuestionButton);
+            this.questionPanel.Controls.Add(this.editQuestionButton);
+            this.questionPanel.Controls.Add(this.delQuestion);
             this.questionPanel.Controls.Add(this.saveQuestion);
             this.questionPanel.Controls.Add(this.questionTextBox);
             this.questionPanel.Location = new System.Drawing.Point(244, 3);
             this.questionPanel.Name = "questionPanel";
-            this.questionPanel.Size = new System.Drawing.Size(705, 563);
+            this.questionPanel.Size = new System.Drawing.Size(705, 586);
             this.questionPanel.TabIndex = 0;
+            // 
+            // questionName
+            // 
+            this.questionName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionName.Location = new System.Drawing.Point(439, 3);
+            this.questionName.Name = "questionName";
+            this.questionName.Size = new System.Drawing.Size(263, 35);
+            this.questionName.TabIndex = 10;
+            // 
+            // answersList
+            // 
+            this.answersList.FormattingEnabled = true;
+            this.answersList.ItemHeight = 20;
+            this.answersList.Location = new System.Drawing.Point(7, 346);
+            this.answersList.Name = "answersList";
+            this.answersList.Size = new System.Drawing.Size(319, 224);
+            this.answersList.TabIndex = 9;
+            this.answersList.SelectedIndexChanged += new System.EventHandler(this.answersList_SelectedIndexChanged);
+            // 
+            // questionNameLabel
+            // 
+            this.questionNameLabel.AutoSize = true;
+            this.questionNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionNameLabel.Location = new System.Drawing.Point(258, 6);
+            this.questionNameLabel.Name = "questionNameLabel";
+            this.questionNameLabel.Size = new System.Drawing.Size(187, 29);
+            this.questionNameLabel.TabIndex = 2;
+            this.questionNameLabel.Text = "Question Name:";
+            // 
+            // questionTextLabel
+            // 
+            this.questionTextLabel.AutoSize = true;
+            this.questionTextLabel.Location = new System.Drawing.Point(3, 26);
+            this.questionTextLabel.Name = "questionTextLabel";
+            this.questionTextLabel.Size = new System.Drawing.Size(111, 20);
+            this.questionTextLabel.TabIndex = 2;
+            this.questionTextLabel.Text = "Question Text:";
             // 
             // answerPanel
             // 
-            this.answerPanel.Controls.Add(this.possibleAnswerTextBox);
+            this.answerPanel.Controls.Add(this.possibleAnswerLabel);
             this.answerPanel.Controls.Add(this.correctAnswerBox);
-            this.answerPanel.Controls.Add(this.textBox1);
+            this.answerPanel.Controls.Add(this.possibleAnswerTextBox);
             this.answerPanel.Location = new System.Drawing.Point(332, 311);
             this.answerPanel.Name = "answerPanel";
             this.answerPanel.Size = new System.Drawing.Size(370, 100);
             this.answerPanel.TabIndex = 8;
             // 
-            // possibleAnswerTextBox
+            // possibleAnswerLabel
             // 
-            this.possibleAnswerTextBox.AutoSize = true;
-            this.possibleAnswerTextBox.Location = new System.Drawing.Point(3, 12);
-            this.possibleAnswerTextBox.Name = "possibleAnswerTextBox";
-            this.possibleAnswerTextBox.Size = new System.Drawing.Size(211, 20);
-            this.possibleAnswerTextBox.TabIndex = 6;
-            this.possibleAnswerTextBox.Text = "Enter Possible Answer Here:";
+            this.possibleAnswerLabel.AutoSize = true;
+            this.possibleAnswerLabel.Location = new System.Drawing.Point(3, 12);
+            this.possibleAnswerLabel.Name = "possibleAnswerLabel";
+            this.possibleAnswerLabel.Size = new System.Drawing.Size(211, 20);
+            this.possibleAnswerLabel.TabIndex = 6;
+            this.possibleAnswerLabel.Text = "Enter Possible Answer Here:";
             // 
             // correctAnswerBox
             // 
@@ -161,12 +216,12 @@
             this.correctAnswerBox.Text = "Correct Answer?";
             this.correctAnswerBox.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // possibleAnswerTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(7, 35);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(347, 26);
-            this.textBox1.TabIndex = 5;
+            this.possibleAnswerTextBox.Location = new System.Drawing.Point(7, 35);
+            this.possibleAnswerTextBox.Name = "possibleAnswerTextBox";
+            this.possibleAnswerTextBox.Size = new System.Drawing.Size(347, 26);
+            this.possibleAnswerTextBox.TabIndex = 5;
             // 
             // possibleAnswersLabel
             // 
@@ -177,31 +232,78 @@
             this.possibleAnswersLabel.TabIndex = 4;
             this.possibleAnswersLabel.Text = "Possible Answers:";
             // 
-            // possibleAnswers
+            // editAnswerButton
             // 
-            this.possibleAnswers.Location = new System.Drawing.Point(3, 339);
-            this.possibleAnswers.Name = "possibleAnswers";
-            this.possibleAnswers.Size = new System.Drawing.Size(312, 221);
-            this.possibleAnswers.TabIndex = 3;
-            this.possibleAnswers.UseCompatibleStateImageBehavior = false;
+            this.editAnswerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editAnswerButton.Location = new System.Drawing.Point(339, 456);
+            this.editAnswerButton.Name = "editAnswerButton";
+            this.editAnswerButton.Size = new System.Drawing.Size(172, 38);
+            this.editAnswerButton.TabIndex = 2;
+            this.editAnswerButton.Text = "Edit Answer";
+            this.editAnswerButton.UseVisualStyleBackColor = true;
+            this.editAnswerButton.Click += new System.EventHandler(this.editAnswer_Click);
+            // 
+            // delAnswer
+            // 
+            this.delAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delAnswer.Location = new System.Drawing.Point(339, 500);
+            this.delAnswer.Name = "delAnswer";
+            this.delAnswer.Size = new System.Drawing.Size(172, 38);
+            this.delAnswer.TabIndex = 2;
+            this.delAnswer.Text = "Delete Answer";
+            this.delAnswer.UseVisualStyleBackColor = true;
+            this.delAnswer.Click += new System.EventHandler(this.delAnswer_Click);
             // 
             // addAnswer
             // 
             this.addAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addAnswer.Location = new System.Drawing.Point(517, 474);
+            this.addAnswer.Location = new System.Drawing.Point(339, 544);
             this.addAnswer.Name = "addAnswer";
-            this.addAnswer.Size = new System.Drawing.Size(185, 38);
+            this.addAnswer.Size = new System.Drawing.Size(172, 38);
             this.addAnswer.TabIndex = 2;
             this.addAnswer.Text = "Add Answer";
             this.addAnswer.UseVisualStyleBackColor = true;
             this.addAnswer.Click += new System.EventHandler(this.addAnswer_Click);
             // 
+            // newQuestionButton
+            // 
+            this.newQuestionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newQuestionButton.Location = new System.Drawing.Point(530, 406);
+            this.newQuestionButton.Name = "newQuestionButton";
+            this.newQuestionButton.Size = new System.Drawing.Size(172, 42);
+            this.newQuestionButton.TabIndex = 1;
+            this.newQuestionButton.Text = "New Question";
+            this.newQuestionButton.UseVisualStyleBackColor = true;
+            this.newQuestionButton.Click += new System.EventHandler(this.newQuestion_Click);
+            // 
+            // editQuestionButton
+            // 
+            this.editQuestionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editQuestionButton.Location = new System.Drawing.Point(530, 454);
+            this.editQuestionButton.Name = "editQuestionButton";
+            this.editQuestionButton.Size = new System.Drawing.Size(172, 42);
+            this.editQuestionButton.TabIndex = 1;
+            this.editQuestionButton.Text = "Edit Question";
+            this.editQuestionButton.UseVisualStyleBackColor = true;
+            this.editQuestionButton.Click += new System.EventHandler(this.editQuestion_Click);
+            // 
+            // delQuestion
+            // 
+            this.delQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delQuestion.Location = new System.Drawing.Point(530, 498);
+            this.delQuestion.Name = "delQuestion";
+            this.delQuestion.Size = new System.Drawing.Size(172, 42);
+            this.delQuestion.TabIndex = 1;
+            this.delQuestion.Text = "Delete Question";
+            this.delQuestion.UseVisualStyleBackColor = true;
+            this.delQuestion.Click += new System.EventHandler(this.delQuestion_Click);
+            // 
             // saveQuestion
             // 
             this.saveQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveQuestion.Location = new System.Drawing.Point(517, 518);
+            this.saveQuestion.Location = new System.Drawing.Point(530, 544);
             this.saveQuestion.Name = "saveQuestion";
-            this.saveQuestion.Size = new System.Drawing.Size(185, 42);
+            this.saveQuestion.Size = new System.Drawing.Size(172, 38);
             this.saveQuestion.TabIndex = 1;
             this.saveQuestion.Text = "Save Question";
             this.saveQuestion.UseVisualStyleBackColor = true;
@@ -209,16 +311,16 @@
             // 
             // questionTextBox
             // 
-            this.questionTextBox.Location = new System.Drawing.Point(3, 3);
+            this.questionTextBox.Location = new System.Drawing.Point(3, 51);
             this.questionTextBox.Name = "questionTextBox";
-            this.questionTextBox.Size = new System.Drawing.Size(699, 302);
+            this.questionTextBox.Size = new System.Drawing.Size(699, 254);
             this.questionTextBox.TabIndex = 0;
             this.questionTextBox.Text = "";
             // 
             // cancelButton
             // 
             this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cancelButton.Location = new System.Drawing.Point(596, 685);
+            this.cancelButton.Location = new System.Drawing.Point(592, 709);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(230, 50);
             this.cancelButton.TabIndex = 3;
@@ -229,7 +331,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(978, 744);
+            this.ClientSize = new System.Drawing.Size(978, 771);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.questionControls);
             this.Controls.Add(this.saveButton);
@@ -256,17 +358,25 @@
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.Panel questionControls;
         private System.Windows.Forms.Panel questionPanel;
-        private System.Windows.Forms.Label possibleAnswerTextBox;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label possibleAnswerLabel;
+        private System.Windows.Forms.TextBox possibleAnswerTextBox;
         private System.Windows.Forms.Label possibleAnswersLabel;
-        private System.Windows.Forms.ListView possibleAnswers;
         private System.Windows.Forms.Button addAnswer;
         private System.Windows.Forms.Button saveQuestion;
         private System.Windows.Forms.RichTextBox questionTextBox;
         private System.Windows.Forms.Panel answerPanel;
         private System.Windows.Forms.CheckBox correctAnswerBox;
         private System.Windows.Forms.Label questionsListLabel;
-        private System.Windows.Forms.ListView questionsList;
         private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ListBox questionsList;
+        private System.Windows.Forms.ListBox answersList;
+        private System.Windows.Forms.TextBox questionName;
+        private System.Windows.Forms.Label questionNameLabel;
+        private System.Windows.Forms.Label questionTextLabel;
+        private System.Windows.Forms.Button delAnswer;
+        private System.Windows.Forms.Button delQuestion;
+        private System.Windows.Forms.Button editAnswerButton;
+        private System.Windows.Forms.Button editQuestionButton;
+        private System.Windows.Forms.Button newQuestionButton;
     }
 }
